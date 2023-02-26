ccb3354d16185c27acef92e25de7aecb47076826
tidjini <tidjini.messaoudi@gmail.com>
2023-02-26 21:42:25 +0100

    Skip to test another firstname

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -8,15 +8,21 @@ describe("Appointements", () => {
 
   beforeEach(() => {
     container = document.createElement("div");
-    document.body.replaceChildren(container);
+    document.body.appendChild(container);
   });
   const render = (component) =>
     act(() => ReactDOM.createRoot(container).render(component));
 
   it("render the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    render(<Appointment />);
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toMatch("Ashley");
+  });
+
+  it.skip("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
+    expect(document.body.textContent).toMatch("Jordan");
   });
 });
