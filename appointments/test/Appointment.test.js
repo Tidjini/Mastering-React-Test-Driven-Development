3833b3eb686d9cd2ef43261cd34419be3db49274
tidjini <tidjini.messaoudi@gmail.com>
2023-02-26 20:13:54 +0100

    Test with JSDOM

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,3 +1,4 @@
+//jest use jsdom a headless implementation of the DOM
 describe("Appointements", () => {
   it("render the customer first name", () => {
     expect(document.body.textContent).toMatch("Ashley");
