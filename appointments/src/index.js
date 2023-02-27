313d958f4d7b9cafe37237c414ca80439528cf5d
tidjini <tidjini.messaoudi@gmail.com>
2023-02-27 21:57:04 +0100

    build with success

--- appointments/src/index.js
+++ appointments/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppointmentsDayView } from "./Appointment";
+import { AppointmentsDayView } from "./Appointment.jsx";
 import { sampleAppointments } from "./sampleData";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
