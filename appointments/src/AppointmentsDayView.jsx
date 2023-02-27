399d820e1f2dc8dba8c8b079d9041af4022548f3
tidjini <tidjini.messaoudi@gmail.com>
2023-02-27 22:16:09 +0100

    set table in appointment

--- appointments/src/AppointmentsDayView.jsx
+++ appointments/src/AppointmentsDayView.jsx
@@ -4,7 +4,36 @@ const appointmentTimeOfDay = (startAt) => {
   const [h, m] = new Date(startAt).toTimeString().split(":");
   return `${h}:${m}`;
 };
-export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+export const Appointment = ({ customer }) => {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>Customer</td>
+            <td>{customer.firstName}</td>
+          </tr>
+          <tr>
+            <td>Phone number</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Stylist</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Service</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Notes</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export const AppointmentsDayView = ({ appointments }) => {
   const [appointment, setAppointment] = React.useState(0);
