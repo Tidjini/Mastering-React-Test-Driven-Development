e1c818d1d5520a798eb52b1bd4618f9357b829f4
tidjini <tidjini.messaoudi@gmail.com>
2023-03-01 20:48:29 +0100

    check firstname field as textbox

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -14,4 +14,11 @@ describe("CustomerForm", () => {
     render(<CustomerForm />);
     expect(form("customer")).not.toBeNull();
   });
+  it("renders the first name field as a textbox", () => {
+    render(<CustomerForm />);
+    const field = form("customer").elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual("INPUT");
+    expect(field.type).toEqual("text");
+  });
 });
