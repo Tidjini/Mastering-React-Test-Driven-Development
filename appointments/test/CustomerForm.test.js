c15b82a681bf7e0917a725f29144c257e11f7b0a
tidjini <tidjini.messaoudi@gmail.com>
2023-03-01 20:53:47 +0100

    Extacting Expect Input Text Field

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -10,6 +10,11 @@ describe("CustomerForm", () => {
   });
   const form = (id) => container.querySelector(`form[id=${id}]`);
 
+  const expectToBeInputFieldOfTypeText = (formElement) => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual("INPUT");
+    expect(formElement.type).toEqual("text");
+  };
   it("renders a form", () => {
     render(<CustomerForm />);
     expect(form("customer")).not.toBeNull();
@@ -17,8 +22,6 @@ describe("CustomerForm", () => {
   it("renders the first name field as a textbox", () => {
     render(<CustomerForm />);
     const field = form("customer").elements.firstName;
-    expect(field).not.toBeNull();
-    expect(field.tagName).toEqual("INPUT");
-    expect(field.type).toEqual("text");
+    expectToBeInputFieldOfTypeText(field);
   });
 });
